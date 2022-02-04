ab78e726a16f966b58d8bea24bd4530dc399e8c1
KjetilMainPc <kjetilindrehus@gmail.com>
2022-02-04 10:50:31 +0100

    Added functions for backend JS

--- ChallengeProjectNumberGuesser/script.js
+++ ChallengeProjectNumberGuesser/script.js
@@ -4,3 +4,28 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 
+function generateTarget(){
+    return Math.floor(Math.random()*9); // random target between 0-9
+}
+
+
+function compareGuesses(human,computer, target){ //return true if the player wins 
+    if(Math.abs(human-target)<= Math.abs(computer-target)){
+        return true;
+    }
+    return false;
+
+}
+
+function updateScore(str){ //update score based on str input (either human or computer)
+    if(str === "human"){
+        humanScore++;
+    }else{
+        computerScore++;
+    }
+}
+
+function advanceRound(){
+    currentRoundNumber++;
+}
+
